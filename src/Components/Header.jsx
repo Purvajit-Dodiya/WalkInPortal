d52fe696b484df1a740e57cb3473412992a1c6b8
purvajit <purvajit.dodiya@zeuslearning.com>
2024-02-09 19:53:20 +0530

    style chage in header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import logo from "../icons/Zeus-LMS-logo.svg"
+import logo from "../icons/Zeus-LMS-logo.svg";
+import userImg from "../icons/customer_man_user_account_profile_icon.svg";
 
 export default function Header() {
+  const authenticated = true; //handle user authentication
   return (
     <header className="header">
       <img src={logo} className="header_logo" alt="LOGO" />
+      <div className="user_profile">
+        <img src={userImg} alt="User Profile" />
+      </div>
     </header>
   );
 }
